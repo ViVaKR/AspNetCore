985ea9b23ad0a649f1f3dabfc923818d02e5dfba
Kim Bum Jun <admin@vivabj.com>
2024-03-15 19:47:05 +0900

    remove hidden folder

---
+++ APIs/GameStore/Todo.Api/Controllers/TodoAppController.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MyApp.Namespace
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TodoAppController : ControllerBase
+    {
+        //
+    }
+}
---
+++ APIs/GameStore/Todo.Api/Program.cs
@@ -0,0 +1,36 @@
+using Newtonsoft.Json.Serialization;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+// JSON Serializer
+builder.Services.AddControllers().AddNewtonsoftJson(x =>
+x.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore)
+.AddNewtonsoftJson(x => x.SerializerSettings.ContractResolver = new DefaultContractResolver());
+
+var app = builder.Build();
+
+// Enable CORS
+app.UseCors(x => x.AllowAnyHeader().AllowAnyOrigin().AllowAnyMethod());
+
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
